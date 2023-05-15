1634d2435b95e27b26a775bc5a92b9ae16cedb99
al.shibalkin <al.shibalkin@ya.ru>
2023-05-15 22:36:26 +0500

    Добавлен разрешенный хост

--- learndjango/learndjango/settings.py
+++ learndjango/learndjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qarom2q9w)_#9ua2@nphu06cpwyerlukyfqw*gs0q01vgw82q6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['AlexZeeKer.pythonanywhere.com']
 
 
 # Application definition
